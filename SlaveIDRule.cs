bdf225a9a32d53968a58d124730414cc95daae82
tendrbeik <abdramanov-2014@mail.ru>
2026-04-19 19:30:34 +0300

    Добавил класс SlaveIDRule для валидации значений поля SlaveID.

---
+++ SlaveIDRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PressureEmulationWPF
+{
+    internal class SlaveIDRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var resultValue;
+            try
+            {
+                if (((string)value).Length == 0)
+                    return new ValidationResult(false, $"SlaveID не может быть пустой строкой!");
+                if (((string)value).Length > 0)
+                    //TODO: Надо разобраться сколько SlaveID вообще может быть на одном сервере.
+                    resultValue = byte.Parse((string)value);
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, $"Неправильные символы или ошибка \"{e.Message}\". Используйте формат ввода SlaveID - \"1\".");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
