37d36b8f57f2dbffdd253aa654fdef7591d60102
tendrbeik <abdramanov-2014@mail.ru>
2026-04-21 23:44:36 +0300

    Добавил класс RegisterData для хранения записи о регистре Modbus слейва.

---
+++ Model/RegisterData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PressureEmulationWPF.Model
+{
+    internal class RegisterData<T>
+    {
+        public string? Address  { get; set; }
+        public T? RegisterValue { get; set; }
+    }
+}
